4c5e21008574ae654c03ec062e4ffc3bae1b14b9
Okumu16 <2200801100@std.kyu.ac.ug>
2023-11-14 21:43:30 +0300

    trial6

--- my_printc.c
+++ my_printc.c
@@ -11,14 +11,41 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int len = 0;
-
-	if (format == NULL)
-		return (-1);
+	int count = 0;
 
 	va_start(args, format);
 
-	len = _print_format(format, args);
-	va_end(args);
-	return (len);
+for (int i = 0; format[i] != '\0'; i++)
+{
+	if (format[i] != '%')
+	{
+		write(1, &format[i], 1);
+		count++;
+			}
+			else
+			{
+				i++;
+				switch (format[i])
+				{
+					case 'c':
+					write(1, va_arg(args, int), 1);
+					count++;
+					break;
+					case 's':
+					for (int j = 0; va_arg(args, char *)[j] != '\0'; j++)
+					{
+						write(1, &va_arg(args, char *)[j], 1);
+						count++;
+						}
+						break;
+						case '%':
+						write(1, &format[i], 1);
+						count++;
+						break;
+						}
+						}
+						}
+						va_end(args);
+
+return (count);
 }
